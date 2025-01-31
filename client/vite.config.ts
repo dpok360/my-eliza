4daff3f6d05c22cd0f012208b5c2df013607172e
DEEPAK MAC <deepakmac@DEEPAKs-MacBook-Air.local>
2025-01-31 12:49:04 +0545

    setup vite compression

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 1024,
+    }),
+  ],
 });
